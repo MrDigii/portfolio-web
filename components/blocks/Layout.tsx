556875d36f4f1690c6e99a6661f70b4cd9c9b98b
MrDigii <lukas@wochner.me>
2026-04-17 00:42:05 +0200

    feat: adding ref forwarding for layout components

--- components/blocks/Layout.tsx
+++ components/blocks/Layout.tsx
@@ -1,33 +1,47 @@
-import { FC, ReactNode } from 'react';
+import { FC, forwardRef, ReactNode } from 'react';
 import { cn } from '@/lib/utils';
 
 const Layout: FC<{ children?: ReactNode }> = ({ children }) => {
     return children;
 };
 
-const Header: FC<{ className?: string; children?: ReactNode }> = ({
-    className,
-    children,
-}) => {
+const Header = forwardRef<
+    HTMLElement,
+    { className?: string; children?: ReactNode }
+>(({ className, children }, ref) => {
     return (
-        <header className={cn('layout__header', className)}>{children}</header>
+        <header ref={ref} className={cn('layout__header', className)}>
+            {children}
+        </header>
     );
-};
+});
 
-const Main: FC<{ className?: string; children?: ReactNode }> = ({
-    className,
-    children,
-}) => {
-    return <main className={cn('layout__main', className)}>{children}</main>;
-};
+Header.displayName = 'Header';
 
-const Footer: FC<{ className?: string; children?: ReactNode }> = ({
-    className,
-    children,
-}) => {
+const Main = forwardRef<
+    HTMLElement,
+    { className?: string; children?: ReactNode }
+>(({ className, children }, ref) => {
     return (
-        <footer className={cn('layout__footer', className)}>{children}</footer>
+        <main ref={ref} className={cn('layout__main', className)}>
+            {children}
+        </main>
     );
-};
+});
+
+Main.displayName = 'Main';
+
+const Footer = forwardRef<
+    HTMLElement,
+    { className?: string; children?: ReactNode }
+>(({ className, children }, ref) => {
+    return (
+        <footer ref={ref} className={cn('layout__footer', className)}>
+            {children}
+        </footer>
+    );
+});
+
+Footer.displayName = 'Footer';
 
 export { Layout as View, Main, Header, Footer };
